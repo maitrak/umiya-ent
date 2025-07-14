79fd894c8bef5d12b6e80ec08d06b35788e516e2
maitrak <maitrak523@gmail.com>
2025-07-14 22:24:09 +0530

    logout issue solved

--- src/components/user-button.tsx
+++ src/components/user-button.tsx
@@ -1,44 +1,37 @@
-import { useSession, signOut } from "next-auth/react";
+"use client";
 
 import { useRouter } from "next/navigation";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useSession, signOut } from "next-auth/react";
+import { useEffect } from "react";
+import { Loader } from "lucide-react"; // Or your loader import
 import {
   DropdownMenu,
+  DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Loader } from "lucide-react";
-import { useEffect } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const UserButton = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
-  
-  
-  useEffect(() => {
-    if (status !== "authenticated") {
-          handleSignOut();
-      }
-      
-  }, [status]);
+
+  const avatarFallback = session?.user?.name?.charAt(0).toUpperCase();
+
+const handleSignOut = async () => {
+  await signOut({ callbackUrl: "/" });
+};
+
   if (status === "loading") {
     return <Loader className="size-6 mr-4 mt-4 float-right animate-spin" />;
   }
 
-  const avatarFallback = session?.user?.name?.charAt(0).toUpperCase();
-
-  const handleSignOut = async () => {
-    await signOut({ redirect: false });
-    router.push("/");
-  };
-
   return (
     <>
       {session && (
-        <nav className="flex bg-gray-200 justify-end rounded-3xl items-center h-12 max-w-md mx-auto  shadow border border-gray-300 overflow-hidden">
+        <nav className="flex bg-gray-200 justify-end rounded-3xl items-center h-12 max-w-md mx-auto shadow border border-gray-300 overflow-hidden">
           <DropdownMenu modal={false}>
-            <DropdownMenuTrigger className="outline-none relative float-right  md:p-2">
+            <DropdownMenuTrigger className="outline-none relative float-right md:p-2">
               <div className="bg-[#6f6f6f85] flex justify-between items-center rounded-full w-[150px] pl-[7px]">
                 <span>{session.user?.name}</span>
                 <Avatar className="size-10 hover:opacity-75 transition">
